751817f7e519b851945076883e267d5fcf455878
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-07-20 23:31:05 +0200

    Creating data layer. closed #2

--- src/main/java/com/gordonmzizi/persistance/DataAccessPreProd.java
+++ src/main/java/com/gordonmzizi/persistance/DataAccessPreProd.java
@@ -3,9 +3,16 @@ package com.gordonmzizi.persistance;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import javax.sql.DataSource;
+
 public class DataAccessPreProd implements DataAccess{
     @Value("${preprod}")
     private String preprod;
+    private DataSource dataSource;
+
+    public DataAccessPreProd(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     public void hello() {
--- src/main/java/com/gordonmzizi/persistance/DataAccessProd.java
+++ src/main/java/com/gordonmzizi/persistance/DataAccessProd.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 public class DataAccessProd implements DataAccess {
     @Value("${prod}")
     private String prod;
+    private DataAccess dataAccess;
+
+    public DataAccessProd(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
 
     @Override
     public void hello(){
